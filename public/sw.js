df205751b19461e9bd92e882ff807dada84d0caf
Usama Tahir <usamatahir8330@gmail.com>
2020-09-08 19:59:49 +0500

    Edit service worker file

--- public/sw.js
+++ public/sw.js
@@ -1,39 +1,29 @@
 var StaticCaches = "offline-caches";
 
-this.addEventListener('install', function (event) {
+self.addEventListener('install', function (event) {
     event.waitUntil(
         caches.open(StaticCaches).then(function (cache) {
             return cache.addAll([
-                '/',
-                '../src/index.tsx',
-                '../src/App.tsx',
-                '../src/Style.css',
-                '/index.html',
-                '/static/js/bundle.js',
                 '/static/js/main.chunk.js',
-                '/static/js/1.chunk.js',
-                'http://dasfelynsaterr.webcam/shows/?key=Bf6HGZm06qPjMSd2rqGo8SBJcGRu&ukey=MTM1MzkgD1_LU1S68cRBsL4BFHW56g~~&count=1&',
-                'http://dasfelynsaterr.webcam/shows/?key=YA-298f_wfCUwTL3yYSDMyA2ZFpp&ukey=MTM1MzkgD1_LU1S68cRBsL4BFHW56g~~&count=1&',
-                'https://dasfelynsaterr.webcam/geolocation/',
-                'https://dasfelynsaterr.webcam/geolocation/2197/',
-                'https://1986635568.rsc.cdn77.org/offers/domainList.json',
-                '/logo192.png',
+                '/static/js/0.chunk.js',
+                '/static/js/bundle.js',
                 'https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@1,700&display=swap',
                 'https://fonts.googleapis.com/css2?family=Acme&display=swap',
                 'https://fonts.gstatic.com/s/acme/v10/RrQfboBx-C5_XxrBbk_m3Lg.woff2',
-                'https://theapple.site/cu/g180.js', 
-                '/manifest.json',
-                '/favicon.ico'
+                '/index.html',
+                '/'
             ]);
         })
     );
 })
 
-this.addEventListener('fetch', function (event) {
+self.addEventListener('fetch', function (event) {
     
     event.respondWith(
         caches.match(event.request).then(function (response) {
-            if (response) return response;
+            if (response) {
+                return response;
+            }
             return fetch(event.request);
         })
     );
@@ -41,7 +31,7 @@ this.addEventListener('fetch', function (event) {
 });
 
 
-this.addEventListener('activate', function (event) {
+self.addEventListener('activate', function (event) {
     event.waitUntil(
         caches.keys().then(function (cachesName) {
             return Promise.all(
